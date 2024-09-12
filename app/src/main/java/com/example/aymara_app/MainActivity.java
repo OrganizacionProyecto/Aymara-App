ee5a828541940a0939aaacbf57208a51308dfa59
Antonella <antonellacosta_93@hotmail.com>
2024-09-12 01:50:29 -0300

    creando PrimerFragment y realizando pruebas en diseño xml.

--- app/src/main/java/com/example/aymara_app/MainActivity.java
+++ app/src/main/java/com/example/aymara_app/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.aymara_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameContainer, new PrimerFragment());
+        transaction.setReorderingAllowed(true);
+        transaction.addToBackStack("replacement");
+        transaction.commit();
     }
 }
