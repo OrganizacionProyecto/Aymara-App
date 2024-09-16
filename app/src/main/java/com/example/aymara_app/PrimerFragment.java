073e540fcb605c7d2f2705f174d9caa5bc932790
Antonella <antonellacosta_93@hotmail.com>
2024-09-16 11:08:27 -0300

    modificaciones en PrimerFragment para visualizar productos

--- app/src/main/java/com/example/aymara_app/PrimerFragment.java
+++ app/src/main/java/com/example/aymara_app/PrimerFragment.java
@@ -1,16 +1,19 @@
 package com.example.aymara_app;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,11 +63,29 @@ public class PrimerFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primer, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflar la vista del fragmento
+        View view = inflater.inflate(R.layout.fragment_primer, container, false);
+
+        // Inicializar el RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_products);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Crear una lista de productos de ejemplo
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("Aceite de Cocos", "Suplemento dietario a base de aceite de coco. 100% natural", R.drawable.aceite_coco));
+        productList.add(new Product("Garcimax Slim", " Suplemento dietario natural de heirbas (garcinia cambogia, fucus vesiculoso, té verde y café verde) y vitamina B1", R.drawable.garcimax));
+        productList.add(new Product("Café Verde Plus", "Suplemento dietario a base de café verde, vitamina B6, L-carnitina, té verde y garcinia cambogia", R.drawable.cafe_verde));
+
+
+
+        // Imprimir el tamaño de la lista en la consola para verificar
+        Log.d("PrimerFragment", "Tamaño de la lista de productos: " + productList.size());
+
+        // Configurar el adaptador
+        ProductAdapter productAdapter = new ProductAdapter(productList);
+        recyclerView.setAdapter(productAdapter);
+
+        return view;
     }
 }
-
-
