e4af55e57f28110e342a69c84ba6615a83c19a4f
JoaquinCamino12 <joaquincamino09@gmail.com>
2024-09-16 16:57:16 -0300

    Se agrego un slider de imagenes a home

--- app/src/main/java/com/example/aymara_app/HomeFragment.java
+++ app/src/main/java/com/example/aymara_app/HomeFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 public class HomeFragment extends Fragment {
 
@@ -12,10 +15,21 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Configura el ViewPager para el carrusel de imágenes
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        ImageSliderAdapter adapter = new ImageSliderAdapter(getContext());
+        viewPager.setAdapter(adapter);
     }
 }
