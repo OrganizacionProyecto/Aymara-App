d9ea3f2a0ff903128b2f6b0a4d00aa24d29683b2
JoaquinCamino12 <joaquincamino09@gmail.com>
2024-09-16 16:02:55 -0300

    Funcionalidad de navbar y proceso de construccion de home.

--- app/src/main/java/com/example/aymara_app/MainActivity.java
+++ app/src/main/java/com/example/aymara_app/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.aymara_app;
-import com.example.aymara_app.PrimerFragment;
+import com.example.aymara_app.R;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.example.aymara_app.HomeFragment;
+import com.example.aymara_app.PrimerFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +18,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frameContainer, new PrimerFragment());
-        transaction.setReorderingAllowed(true);
-        transaction.addToBackStack("replacement");
-        transaction.commit();
+        // Inicializa el fragmento por defecto
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameContainer, new HomeFragment())
+                    .commit();
+        }
+
+        // barra de navegación inferior
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                // Seleccionar fragmento según el ítem clickeado
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_products:
+                        selectedFragment = new PrimerFragment();
+                        break;
+                //    case R.id.nav_contact:
+                 //       selectedFragment = new ContactFragment();
+                   //     break;
+                   // case R.id.nav_user:
+                     //   selectedFragment = new ProfileFragment();
+                       // break;
+                }
+
+                // Reemplaza el fragmento actual con el seleccionado
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameContainer, selectedFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+                return true;
+            }
+        });
     }
 }
