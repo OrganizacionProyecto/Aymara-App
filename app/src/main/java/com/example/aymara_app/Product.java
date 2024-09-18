ea35a84173fe4431971cac31897f51f6d02ab65c
Antonella <antonellacosta_93@hotmail.com>
2024-09-17 23:00:40 -0300

    se suben product.java

---
+++ app/src/main/java/com/example/aymara_app/Product.java
@@ -0,0 +1,25 @@
+package com.example.aymara_app;
+
+public class Product {
+    private String name;
+    private String description;
+    private int imageResId;
+
+    public Product(String name, String description, int imageResId) {
+        this.name = name;
+        this.description = description;
+        this.imageResId = imageResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+}
