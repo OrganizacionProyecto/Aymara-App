dafd21d7b54f51d5063c86c4b5b19d2144ab95ff
Walter <106196739+waltercamino@users.noreply.github.com>
2024-09-23 19:56:04 -0300

    agregando ojito al login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.8.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
