eb1a1386fa2bab4920e34f88ca777f6e2383b2ee
JoaquinCamino12 <joaquincamino09@gmail.com>
2024-10-17 19:05:06 -0300

    Se implemento CRUD de ProfileFragment.java

--- app/src/main/java/com/example/aymara_app/MainActivity.java
+++ app/src/main/java/com/example/aymara_app/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.aymara_app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,33 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializa el NavHostFragment y el NavController
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
 
-        // Configura la barra de navegación inferior
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            // Seleccionar fragmento según el ítem clickeado
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-                    navController.navigate(R.id.HomeFragment);
-                    return true;
-                case R.id.nav_products:
-                    navController.navigate(R.id.ProductFragment);
-                    return true;
-                case R.id.nav_contac:
-                    navController.navigate(R.id.ContacFragment);
-                    return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> navigateToFragment(item.getItemId(), navController));
+    }
 
-                case R.id.nav_user:
-                    navController.navigate(R.id.loginFragment);
-                    return true;
-                case R.id.nav_profile:
+    private boolean navigateToFragment(int itemId, NavController navController) {
+        switch (itemId) {
+            case R.id.nav_home:
+                navController.navigate(R.id.HomeFragment);
+                return true;
+            case R.id.nav_products:
+                navController.navigate(R.id.ProductFragment);
+                return true;
+            case R.id.nav_contac: // Corregido aquí
+                navController.navigate(R.id.ContacFragment);
+                return true;
+            case R.id.nav_user:
+                if (isUserLoggedIn()) {
                     navController.navigate(R.id.profileFragment);
-                    return true;
-            }
-            return false;
-        });
+                } else {
+                    navController.navigate(R.id.loginFragment);
+                }
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isUserLoggedIn() {
+        SharedPreferences prefs = getSharedPreferences("AymaraPrefs", Context.MODE_PRIVATE);
+        return prefs.getBoolean("is_logged_in", false); // Retorna true si está logueado
     }
 }
