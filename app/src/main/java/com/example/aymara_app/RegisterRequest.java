b1df44a349267b3bb00e0bcd1d98719b150df968
Maria Belen Colado <129967991+MariaBelen1@users.noreply.github.com>
2024-10-18 15:57:48 -0300

    se agrego actualizacion de register

---
+++ app/src/main/java/com/example/aymara_app/RegisterRequest.java
@@ -0,0 +1,53 @@
+package com.example.aymara_app;
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String username;
+    private String first_name;
+    private String last_name;
+
+    private boolean is_active = true;
+    private boolean is_staff = false;
+    private boolean is_superuser = false;
+
+    public RegisterRequest(String email, String password, String username, String first_name, String last_name) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public boolean isActive() {
+        return is_active;
+    }
+
+    public boolean isStaff() {
+        return is_staff;
+    }
+
+    public boolean isSuperuser() {
+        return is_superuser;
+    }
+}
