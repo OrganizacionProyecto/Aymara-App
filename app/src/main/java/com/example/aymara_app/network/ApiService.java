f1326ee42155ccc688459c5a066e52fcc84f15fa
Cecilia Cogot <ceciliacogot@gmail.com>
2025-06-02 19:44:49 -0300

    boton para historial de compras/pedidos

--- app/src/main/java/com/example/aymara_app/network/ApiService.java
+++ app/src/main/java/com/example/aymara_app/network/ApiService.java
@@ -81,13 +81,13 @@ public interface ApiService {
         @PATCH("api/users/change-direccion/")
         Call<ResponseBody> changeAddress(@Header("Authorization") String token, @Body Map<String, String> body);
 
-        @PATCH("api/users/change-username/")
+        @PATCH("/api/users/{id}/")
         Call<ResponseBody> changeUsername(@Header("Authorization") String token, @Body Map<String, String> body);
 
         @POST("api/users/change-password/")
         Call<ResponseBody> changePassword(@Header("Authorization") String token, @Body RequestBody body);
 
-        @DELETE("api/users/auth/delete_account/")
+        @DELETE("/api/users/{id}/")
         Call<ResponseBody> deleteAccount(@Header("Authorization") String token);
 
         @POST("api/auth/token/refresh/")
