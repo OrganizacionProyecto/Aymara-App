3b3a1d4d8b6b1adcf0bdfc8386af723715f0e9eb
xNachooox2442 <nachobonzon1@gmail.com>
2025-06-03 22:30:57 -0300

    add PedidoResponse.java

---
+++ app/src/main/java/com/example/aymara_app/PedidoResponse.java
@@ -0,0 +1,14 @@
+package com.example.aymara_app;
+
+public class PedidoResponse {
+    private int id;
+    private String mercadopagoUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMercadopagoUrl() {
+        return mercadopagoUrl;
+    }
+}
